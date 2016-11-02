e357cef1f19f0dadc9fbed945efdfb09e5340b18
josue23 <josuerodrigues@gmail.com>
2016-11-02 15:38:09 -0200

    adicionado .gitignore

---
+++ aula10.js
@@ -0,0 +1,11 @@
+$(function() {
+	var titulo = $('h1');
+	alert($('titulo'));
+	titulo.html('Título novo 2');
+});
+
+var div = document.getElementById('div');
+
+$(function() {
+	// titulo.html('Título novo 2');
+});
