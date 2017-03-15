fa68ecb2af583d4e9cc687d6449d32b2fdd77101
Josue23 <josuerodrigues@gmail.com>
2017-03-15 09:17:57 -0300

    usando o método auxiliar $.post

--- secao5/41-ajax.js
+++ secao5/41-ajax.js
@@ -3,7 +3,7 @@ $(function() {
         // requisita 41.1-ajax.html
         // faz a função
         // armazena o resultado no parâmetro data
-        $.get("41.1-ajax.html", function(data) {
+        $.post("41.1-ajax.html", function(data) {
             $('.div').html(data);
         });
     })
